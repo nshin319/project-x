c85b4d4295c3f3dacd074d52c5b4aaf5cf3a5a6a
nshin319 <n.shindomo0319@gmail.com>
2025-10-06 17:15:53 +0900

    add: backend docker config

---
+++ backend/src/main/java/com/example/project_x/ProjectXApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectXApplication.class, args);
+	}
+
+}
