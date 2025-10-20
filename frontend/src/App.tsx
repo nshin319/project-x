cd312f26d737e21fe3256a1cbb1f208e7be6d637
nshin319 <n.shindomo0319@gmail.com>
2025-10-20 11:04:38 +0900

    update: frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>出退勤管理システム</h1>
+      <p>TypeScript版で動作中！</p>
     </div>
   );
 }
